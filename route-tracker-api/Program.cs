d6be4d247bdb444347f0d1aca39127e215119e3c
László Csizmadia <csizilaci92@gmail.com>
2023-02-11 10:45:02 +0100

    Configure AzureAdB2C service

--- route-tracker-api/Program.cs
+++ route-tracker-api/Program.cs
@@ -5,7 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAdB2C"));
+    .AddMicrosoftIdentityWebApi(options =>
+        {
+            
+            builder.Configuration.Bind("AzureAdB2C", options);
+
+            options.TokenValidationParameters.NameClaimType = "name";
+        },
+        options => { builder.Configuration.Bind("AzureAdB2C", options); });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
