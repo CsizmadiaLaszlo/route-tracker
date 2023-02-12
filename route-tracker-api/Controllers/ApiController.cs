f4c80aae90fa684c765cc22140d3762cd7444bd0
László Csizmadia <csizilaci92@gmail.com>
2023-02-12 15:14:45 +0100

    Add post route to handle new users

--- route-tracker-api/Controllers/ApiController.cs
+++ route-tracker-api/Controllers/ApiController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Security.Cryptography;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using route_tracker_api.Data;
+using route_tracker_api.Services;
+using route_tracker_api.Services.Interfaces;
 
 namespace route_tracker_api.Controllers;
 
@@ -8,6 +12,30 @@ namespace route_tracker_api.Controllers;
 [Route("api")]
 public class ApiController : ControllerBase
 {
+    private readonly IAccountService _accountService;
+
+    public ApiController(IAccountService accountService)
+    {
+        _accountService = accountService;
+    }
+
+    [HttpPost]
+    [Route("new-user")]
+    public async Task<ActionResult> AddNewUser()
+    {
+        var objectId = GetOidForUser();
+        try
+        {
+            await _accountService.AddAccount(objectId);
+        }
+        catch (Exception e)
+        {
+            return Conflict(e);
+        }
+
+        return Ok();
+    }
+    
     private string GetOidForUser()
     {
         const string oidType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier";
