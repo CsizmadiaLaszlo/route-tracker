7f358cac030b075ca19ba4100445d62786ccf137
László Csizmadia <csizilaci92@gmail.com>
2023-02-12 13:12:44 +0100

    Add account model

---
+++ route-tracker-api/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace route_tracker_api.Models;
+
+public class Account
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    private int Id { get; set; }
+    public string ObjectIdentifier { get; set; }
+
+    public Account(string objectIdentifier)
+    {
+        ObjectIdentifier = objectIdentifier;
+    }
+}
