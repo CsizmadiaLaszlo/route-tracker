5b2b98c7e7708512a8bc156a485d198689b83e91
László Csizmadia <csizilaci92@gmail.com>
2023-02-12 13:23:23 +0100

    Add missing summary

--- route-tracker-api/Models/Account.cs
+++ route-tracker-api/Models/Account.cs
@@ -2,10 +2,18 @@
 
 namespace route_tracker_api.Models;
 
+/// <summary>
+/// Represents the user data that is stored on the backend.
+/// </summary>
+
 public class Account
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     private int Id { get; set; }
+    
+    /// <summary>
+    /// ObjectIdentifier is the Object identifier (ID) of the user object in Azure AD
+    /// </summary>
     public string ObjectIdentifier { get; set; }
 
     public Account(string objectIdentifier)
