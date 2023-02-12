ee628ba3e6dcf8f6ba9b46cfe1c5047fc272341c
László Csizmadia <csizilaci92@gmail.com>
2023-02-12 14:18:54 +0100

    Create AccountService.cs

---
+++ route-tracker-api/Services/AccountService.cs
@@ -0,0 +1,18 @@
+﻿using route_tracker_api.Data;
+using route_tracker_api.Services.Interfaces;
+
+namespace route_tracker_api.Services;
+
+/// <summary>
+/// A class dedicated for account related processes.
+/// </summary>
+
+public class AccountService : IAccountService
+{
+    private readonly RouteTrackerApiContext _context;
+
+    public AccountService(RouteTrackerApiContext context)
+    {
+        _context = context;
+    }
+}
