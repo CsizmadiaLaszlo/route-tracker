42e1509d1c6e921655a6b1b7e627d1ad5f7540f6
László Csizmadia <csizilaci92@gmail.com>
2023-02-13 12:22:34 +0100

    Update DbInitializer.cs

--- route-tracker-api/Data/DbInitializer.cs
+++ route-tracker-api/Data/DbInitializer.cs
@@ -1,18 +1,21 @@
 namespace route_tracker_api.Data;
 
+/// <summary>
+/// Ensures database creation on startup and populating it with data.
+/// </summary>
 public static class DbInitializer
 {
     public static void Initialize(RouteTrackerApiContext context)
     {
         // Create DB if not exist
         context.Database.EnsureCreated();
-        
+
         // Look for tables, if any, no action
         if (context.Accounts.Any())
         {
             return;
         }
-        
+
         context.SaveChanges();
     }
 }
