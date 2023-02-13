99dd863b45ca110e7b0fd5849c39f3055881bc4a
László Csizmadia <csizilaci92@gmail.com>
2023-02-13 16:34:59 +0100

    Add DbInitializerTests

---
+++ route-tracker-api-tests/DataTests/DbInitializerTests.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using route_tracker_api.Data;
+using route_tracker_api.Models;
+
+namespace route_tracker_api_tests.DataTests;
+
+[TestFixture]
+public class DbInitializerTests
+{
+    }
+}
