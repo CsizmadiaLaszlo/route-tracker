ad4103cb0f467fdd810072f8fcd3e1ca4a33994d
László Csizmadia <csizilaci92@gmail.com>
2023-02-13 15:56:26 +0100

    Create AccountTests.cs

---
+++ route-tracker-api-tests/ModelTests/AccountTests.cs
@@ -0,0 +1,17 @@
+﻿using route_tracker_api.Models;
+
+namespace route_tracker_api_tests.ModelTests;
+
+[TestFixture]
+public class AccountTests
+{
+    [Test]
+    public void Test_Id_IsSetByDatabase()
+    {
+        // Act
+        var account = new Account();
+        
+        // Assert
+        Assert.That(account.Id, Is.EqualTo(0), "Id should be set by the database");
+    }
+}
