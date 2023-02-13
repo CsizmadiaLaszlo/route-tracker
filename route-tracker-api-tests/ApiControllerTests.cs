abd603484645f87b3e7b9c558fbb1b4824639d0f
László Csizmadia <csizilaci92@gmail.com>
2023-02-13 13:55:46 +0100

    Add NUnit project

---
+++ route-tracker-api-tests/ApiControllerTests.cs
@@ -0,0 +1,16 @@
+namespace route_tracker_api_tests
+{
+    public class ApiControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
