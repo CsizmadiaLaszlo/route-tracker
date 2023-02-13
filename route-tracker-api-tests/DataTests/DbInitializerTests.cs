a3dd29f9ed068bff634db62dd8a295ef188dceaf
László Csizmadia <csizilaci92@gmail.com>
2023-02-13 16:35:32 +0100

    Add Initialization test

--- route-tracker-api-tests/DataTests/DbInitializerTests.cs
+++ route-tracker-api-tests/DataTests/DbInitializerTests.cs
@@ -7,5 +7,21 @@ namespace route_tracker_api_tests.DataTests;
 [TestFixture]
 public class DbInitializerTests
 {
+    [Test]
+    public void TestDbInitialization()
+    {
+        // Arrange
+        var options = new DbContextOptionsBuilder<RouteTrackerApiContext>()
+            .UseInMemoryDatabase(databaseName: "TestDbInitialization")
+            .Options;
+
+        using var context = new RouteTrackerApiContext(options);
+        // Act
+        DbInitializer.Initialize(context);
+
+        // Assert
+        Assert.That(context.Database.EnsureCreated(), Is.False);
+    }
+
     }
 }
