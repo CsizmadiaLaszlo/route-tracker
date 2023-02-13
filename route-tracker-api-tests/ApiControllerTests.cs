2bd9bf98978034c668192508c30a8f163a9e076b
László Csizmadia <csizilaci92@gmail.com>
2023-02-13 14:20:54 +0100

    Add fields

--- route-tracker-api-tests/ApiControllerTests.cs
+++ route-tracker-api-tests/ApiControllerTests.cs
@@ -11,6 +11,10 @@ namespace route_tracker_api_tests
 {
     public class ApiControllerTests
     {
+        private ApiController _controller = null!;
+        private Mock<IAccountService> _accountServiceMock = null!;
+        private Mock<ClaimsPrincipal> _claimsPrincipalMock = null!;
+
         [SetUp]
         public void Setup()
         {
