5d65c7a1ce3aee93081721b681b06cbd65322e6f
László Csizmadia <csizilaci92@gmail.com>
2023-02-14 17:06:08 +0100

    Remove UnauthenticatedTemplate from Home.jsx

--- route-tracker-spa/src/pages/Home.jsx
+++ route-tracker-spa/src/pages/Home.jsx
@@ -5,47 +5,22 @@ import {loginRequest} from "../authConfig.js";
 import {themeChange} from 'theme-change'
 
 export const Home = () => {
-    const {instance} = useMsal();
-    const activeAccount = instance.getActiveAccount();
 
     useEffect(() => {
         themeChange(false)
         // 👆 false parameter is required for react project
     }, [])
 
-    const handleLoginPopup = () => {
-        instance
-            .loginPopup({
-                ...loginRequest,
-                redirectUri: '/',
-            })
-            .catch((error) => console.log(error));
-    };
-
     return (
         <>
-            <AuthenticatedTemplate>
-                {activeAccount ? (
-                    <center>
-                        <button className="btn btn-primary" data-set-theme="corporate">
-                            Change to corporate
-                        </button>
-                        <button className="btn btn-primary" data-set-theme="business">
-                            Change to business
-                        </button>
-                    </center>
-                ) : null}
-            </AuthenticatedTemplate>
-            <UnauthenticatedTemplate>
-                <>
-                    <center>
-                        <h1>You need to login first</h1>
-                        <button className="btn btn-primary" onClick={handleLoginPopup}>
-                            Login
-                        </button>
-                    </center>
-                </>
-            </UnauthenticatedTemplate>
+            <center>
+                <button className="btn btn-primary" data-set-theme="corporate">
+                    Change to corporate
+                </button>
+                <button className="btn btn-primary" data-set-theme="business">
+                    Change to business
+                </button>
+            </center>
         </>
     );
 };
