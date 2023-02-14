5febaa8278614f27c5d52fe5c17f784e26efb6f5
László Csizmadia <csizilaci92@gmail.com>
2023-02-14 20:42:52 +0100

    Remove theme changer from Home

--- route-tracker-spa/src/pages/Home.jsx
+++ route-tracker-spa/src/pages/Home.jsx
@@ -1,7 +1,4 @@
-import {AuthenticatedTemplate, UnauthenticatedTemplate} from '@azure/msal-react';
-import {useMsal} from '@azure/msal-react';
 import {useEffect} from "react";
-import {loginRequest} from "../authConfig.js";
 import {themeChange} from 'theme-change'
 
 export const Home = () => {
@@ -14,12 +11,7 @@ export const Home = () => {
     return (
         <>
             <center>
-                <button className="btn btn-primary" data-set-theme="corporate">
-                    Change to corporate
-                </button>
-                <button className="btn btn-primary" data-set-theme="business">
-                    Change to business
-                </button>
+                Welcome
             </center>
         </>
     );
