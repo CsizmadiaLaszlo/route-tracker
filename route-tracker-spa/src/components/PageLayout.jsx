eeedb5557acc3c879b73e2022c177dcce1d3d733
László Csizmadia <csizilaci92@gmail.com>
2023-02-15 16:38:59 +0100

    Update PageLayout.jsx

--- route-tracker-spa/src/components/PageLayout.jsx
+++ route-tracker-spa/src/components/PageLayout.jsx
@@ -1,24 +1,14 @@
 import {AuthenticatedTemplate, UnauthenticatedTemplate} from '@azure/msal-react';
 import {Outlet} from "react-router-dom";
 import {NavigationBar} from "./NavigationBar.jsx";
+import Container from "./Container.jsx";
 
 export const PageLayout = () => {
     return (
-        <>
-            <AuthenticatedTemplate>
-                <NavigationBar/>
-                <br/>
-                <h5>
-                    <center>RouteTracker testing</center>
-                </h5>
-                <div>
-                    <Outlet/>
-                </div>
-            </AuthenticatedTemplate>
-            <UnauthenticatedTemplate>
-                <Outlet/>
-            </UnauthenticatedTemplate>
-        </>
+        <Container>
+            <NavigationBar/>
+            <Outlet/>
+        </Container>
 
     );
 };
