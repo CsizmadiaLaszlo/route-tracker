589678af0c362095fbf42f85f30963081c95185d
László Csizmadia <csizilaci92@gmail.com>
2023-02-15 17:27:27 +0100

    Replace icon

--- route-tracker-spa/src/components/NavigationBar.jsx
+++ route-tracker-spa/src/components/NavigationBar.jsx
@@ -21,20 +21,7 @@ export const NavigationBar = () => {
                 <Navbar.Start>
                     <Dropdown>
                         <Button color="ghost" shape="circle" tabIndex={0}>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M4 6h16M4 12h16M4 18h7"
-                                />
-                            </svg>
+                            <BiMenuAltLeft/>
                         </Button>
                         <Dropdown.Menu tabIndex={0} className="menu-compact w-52">
                             <Dropdown.Item>Homepage</Dropdown.Item>
