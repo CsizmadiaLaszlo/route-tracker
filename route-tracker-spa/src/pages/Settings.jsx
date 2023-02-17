ddf79b6f69574f791b3716925a1c4cd5f32437cd
László Csizmadia <csizilaci92@gmail.com>
2023-02-17 16:22:15 +0100

    Update Settings page

--- route-tracker-spa/src/pages/Settings.jsx
+++ route-tracker-spa/src/pages/Settings.jsx
@@ -1,13 +1,37 @@
-import {Input, WindowMockup} from "react-daisyui";
+import {Form, Input, Progress, WindowMockup} from "react-daisyui";
 import {useTranslation} from "react-i18next";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import fetchWithToken from "../utils/fetchWithToken.js";
+import {loginRequest} from "../authConfig.js";
+import {useMsal} from "@azure/msal-react";
 
 const Settings = () => {
     const {t} = useTranslation();
+    const {instance} = useMsal();
+    const [loading, setLoading] = useState(true);
     const [hourlyRate, setHourlyRate] = useState(0.00);
     const [overtimeRate, setOvertimeRate] = useState(0.00);
     const [nightShiftRate, setNightShiftRate] = useState(0.00);
 
+    const getTokenSilent = () => {
+        const request = {
+            ...loginRequest,
+            account: instance.getActiveAccount()
+        };
+        return instance.acquireTokenSilent(request);
+    }
+
+    useEffect(() => {
+        getTokenSilent().then((response) => {
+            fetchWithToken(response.accessToken, "GET", "setting")
+                .then(data => {
+                    setHourlyRate(data['hourlyRate']);
+                    setOvertimeRate(data['overtimeRate']);
+                    setNightShiftRate(data['nightShiftRate']);
+                    setLoading(false);
+                })
+        });
+    }, [])
 
     const handleChange = (event) => {
         const target = event.target;
@@ -25,35 +49,44 @@ const Settings = () => {
         }
     }
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = {HourlyRate: hourlyRate, OvertimeRate: overtimeRate, NightShiftRate: nightShiftRate};
+        getTokenSilent().then((response) => {
+            fetchWithToken(response.accessToken, "POST", "setting", data).then();
+        });
+    }
+
     return (
         <>
-            <WindowMockup className={"w-96 container shadow-xl rounded-box bg-base-300"}>
+            <WindowMockup className={"w-80 container shadow-xl rounded-box bg-base-300"}>
                 <div className="flex justify-center px-4  ">
-                    <div className="form-control w-full max-w-xs">
+                    {loading ?
+                        <Progress className="w-56"/>
+                        :
+                        <Form className={"shadow bg-base-200 w-64 rounded-lg p-4"} onSubmit={handleSubmit}>
 
-                        <label className="label">
-                            <span className="label-text">{t('settings.hourlyRate')}</span>
-                        </label>
-                        <Input name={"hourlyRate"} className={"rounded-box"} type={"number"} step={0.01}
-                               value={hourlyRate}
-                               onChange={handleChange}/>
+                            <Form.Label title={t('settings.hourlyRate')}></Form.Label>
+                            <Input name={"hourlyRate"} className={"rounded-box"} type={"number"} step={0.01}
+                                   value={hourlyRate}
+                                   onChange={handleChange}/>
 
-                        <label className="label">
-                            <span className="label-text">{t('settings.overtimeRate')}</span>
-                        </label>
-                        <Input name={"overtimeRate"} className={"rounded-box"} type={"number"} step={0.01}
-                               value={overtimeRate}
-                               onChange={handleChange}/>
+                            <Form.Label title={t('settings.overtimeRate')}></Form.Label>
+                            <Input name={"overtimeRate"} className={"rounded-box"} type={"number"} step={0.01}
+                                   value={overtimeRate}
+                                   onChange={handleChange}/>
 
-                        <label className="label">
-                            <span className="label-text">{t('settings.nightShiftRate')}</span>
-                        </label>
-                        <Input name={"nightShiftRate"} className={"rounded-box bg-base-100"} type={"number"} step={0.01}
-                               value={nightShiftRate}
-                               onChange={handleChange}/>
-                        <br/>
-                        <Input type={"submit"}></Input>
-                    </div>
+                            <Form.Label title={t('settings.nightShiftRate')}></Form.Label>
+                            <Input name={"nightShiftRate"} className={"rounded-box bg-base-100"} type={"number"}
+                                   step={0.01}
+                                   value={nightShiftRate}
+                                   onChange={handleChange}/>
+
+                            <Input className={"rounded-box bg-base-100"} type={"submit"}
+                                   value={t('settings.save')}></Input>
+                        </Form>
+                    }
+
                 </div>
             </WindowMockup>
         </>
