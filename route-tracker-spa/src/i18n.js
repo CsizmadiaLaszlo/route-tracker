031e7bdcee761d627c22e96296fdb110c4abe7b3
László Csizmadia <csizilaci92@gmail.com>
2023-02-18 19:12:25 +0100

    Add date formatter to i18n

--- route-tracker-spa/src/i18n.js
+++ route-tracker-spa/src/i18n.js
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
+import { DateTime } from 'luxon';
 
 export const languages = {
     en: {nativeName: 'English'},
@@ -28,5 +29,9 @@ i18n
             escapeValue: false, // not needed for react as it escapes by default
         }
     }).then();
+
+i18n.services.formatter.add('DATE_HUGE', (value, lng, options) => {
+    return DateTime.fromJSDate(value).setLocale(lng).toLocaleString(DateTime.DATE_HUGE)
+});
 
 export default i18n;
