51a3e24a4e9f7236bacf1f9b0c10257eefb8bfec
László Csizmadia <csizilaci92@gmail.com>
2023-02-20 14:04:24 +0100

    Add RouteSelection component

---
+++ route-tracker-spa/src/components/RouteSelection.jsx
@@ -0,0 +1,23 @@
+import {Badge} from "react-daisyui";
+import {useState} from "react";
+import {useTranslation} from "react-i18next";
+import {mockRoutes} from "../../backEndDataMock.js";
+
+const RouteSelection = ({addNewRoute}) => {
+    const {t} = useTranslation();
+    const [routes, setRoutes] = useState(mockRoutes);
+
+    return (
+        <>
+            {t('home.routeSelectionText')}
+            <div className={"flex flex-wrap justify-center"}>
+                {routes.map((route) => (
+                    <Badge key={route}
+                           className={"m-1 cursor-cell"}
+                           onClick={() => addNewRoute(route)}>{route}</Badge>
+                ))}
+            </div>
+        </>
+    )
+}
+export default RouteSelection;
