6d7f240d672db29cb81c984e2c3a3c773d6f7822
László Csizmadia <csizilaci92@gmail.com>
2023-02-21 14:18:38 +0100

    Remove unused state

--- route-tracker-spa/src/pages/Home.jsx
+++ route-tracker-spa/src/pages/Home.jsx
@@ -9,7 +9,6 @@ import RouteSelection from "../components/RouteSelection.jsx";
 
 export const Home = () => {
     const {t} = useTranslation();
-    const [loading, setLoading] = useState(false);
     const [date, setDate] = useState(new Date());
     const [fromDate, setFromDate] = useState(date);
     const [toDate, setToDate] = useState(date);
@@ -21,34 +20,29 @@ export const Home = () => {
     }
 
     return (
-        <>
-            {loading ?
-                <p>loading</p>
-                :
-                <div className={"flex flex-wrap"}>
-                    <div className={"w-2/3 flex-auto m-1"}>
-                        <MockupWindow titleContainer={<DateSelector date={{date, setDate}}/>}>
-                            <p>placeholder</p>
-                        </MockupWindow>
+        <div className={"flex flex-wrap"}>
+            <div className={"w-2/3 flex-auto m-1"}>
+                <MockupWindow titleContainer={<DateSelector date={{date, setDate}}/>}>
+                    <p>placeholder</p>
+                </MockupWindow>
+            </div>
+            <div className={"w-96 m-1 flex-auto text-center"}>
+                <MockupWindow className={"mt-1"} titleContainer={t('home.newRoute')}>
+                    <div className={"flex flex-wrap mb-5 min-h-16 justify-center"}>
+                        <ShowRoutes routes={selectedRoutes}/>
                     </div>
-                    <div className={"w-96 m-1 flex-auto text-center"}>
-                        <MockupWindow className={"mt-1"} titleContainer={t('home.newRoute')}>
-                            <div className={"flex flex-wrap mb-5 min-h-16 justify-center"}>
-                                <ShowRoutes routes={selectedRoutes}/>
-                            </div>
-                            <div className={"flex m-5 m-h-8 justify-center"}>
-                                <TimeSelector title={t('home.from')} date={fromDate} setDate={setFromDate}/>
-                                <TimeSelector title={t('home.to')} date={toDate} setDate={setToDate}/>
-                            </div>
-                            <div className={"m-5 min-h-8"}>
-                                <NewRouteInput addNewRoute={addNewRouteToSelection}/>
-                            </div>
-                            <div className={"m-5 min-h-8"}>
-                                <RouteSelection addNewRoute={addNewRouteToSelection}/>
-                            </div>
-                        </MockupWindow>
+                    <div className={"flex m-5 m-h-8 justify-center"}>
+                        <TimeSelector title={t('home.from')} date={fromDate} setDate={setFromDate}/>
+                        <TimeSelector title={t('home.to')} date={toDate} setDate={setToDate}/>
                     </div>
-                </div>}
-        </>
+                    <div className={"m-5 min-h-8"}>
+                        <NewRouteInput addNewRoute={addNewRouteToSelection}/>
+                    </div>
+                    <div className={"m-5 min-h-8"}>
+                        <RouteSelection addNewRoute={addNewRouteToSelection}/>
+                    </div>
+                </MockupWindow>
+            </div>
+        </div>
     );
 };
