b6714d42f7cb5cc592950216222cc4f03cd92319
László Csizmadia <csizilaci92@gmail.com>
2023-02-23 17:12:52 +0100

    Refactor main.jsx

--- route-tracker-spa/src/main.jsx
+++ route-tracker-spa/src/main.jsx
@@ -1,31 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 import App from './App'
-import {PublicClientApplication, EventType} from '@azure/msal-browser';
+import {PublicClientApplication} from '@azure/msal-browser';
 import {msalConfig} from "./authConfig.js";
 import './styles/index.css';
 import './i18n';
-import fetchWithToken from "./utils/fetchWithToken.js";
+import msalSetup from "./msal.js";
 
 export const msalInstance = new PublicClientApplication(msalConfig);
-
-
-// Default to using the first account if no account is active on page load
-if (!msalInstance.getActiveAccount() && msalInstance.getAllAccounts().length > 0) {
-    // Account selection logic is app dependent. Adjust as needed for different use cases.
-    msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0]);
-}
-
-msalInstance.addEventCallback((event) => {
-    if (event.eventType === EventType.LOGIN_SUCCESS && event.payload.account) {
-        msalInstance.setActiveAccount(event.payload.account);
-
-        const newUser = event.payload.idTokenClaims.newUser === true
-        if (newUser) {
-            fetchWithToken(msalInstance, "POST", "new-user", null, event.payload.accessToken).then();
-        }
-    }
-});
+msalSetup(msalInstance);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
