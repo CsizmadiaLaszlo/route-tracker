0304050e8e3576071b7b0c5ff5dcda7806b0ed89
László Csizmadia <csizilaci92@gmail.com>
2023-03-01 19:23:06 +0100

    Update NewRouteInput props

--- route-tracker-spa/src/components/home/newRoute/NewRouteInput.jsx
+++ route-tracker-spa/src/components/home/newRoute/NewRouteInput.jsx
@@ -2,7 +2,7 @@ import {Button, Input} from "react-daisyui";
 import {useState} from "react";
 import {useTranslation} from "react-i18next";
 
-const NewRouteInput = ({addNewRoute}) => {
+const NewRouteInput = ({addNewInput}) => {
     const {t} = useTranslation();
     const [newRouteValue, setNewRouteValue] = useState("")
 
@@ -12,7 +12,7 @@ const NewRouteInput = ({addNewRoute}) => {
             <div className={"flex justify-center"}>
                 <Input value={newRouteValue} onChange={(e) => setNewRouteValue(e.target.value)} size={"xs"}/>
                 <Button size={"xs"} onClick={() => {
-                    if (newRouteValue !== "") addNewRoute(newRouteValue);
+                    if (newRouteValue !== "") addNewInput(newRouteValue);
                     setNewRouteValue("");
                 }}>+</Button>
             </div>
