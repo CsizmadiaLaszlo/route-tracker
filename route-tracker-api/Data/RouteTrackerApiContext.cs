53355c9fdc68f02b4aa923f6d91c748e3654d258
László Csizmadia <csizilaci92@gmail.com>
2023-03-02 11:54:41 +0100

    Add Routes to context

--- route-tracker-api/Data/RouteTrackerApiContext.cs
+++ route-tracker-api/Data/RouteTrackerApiContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using route_tracker_api.Models;
+using Route = route_tracker_api.Models.Route;
 
 namespace route_tracker_api.Data;
 
@@ -7,6 +8,7 @@ public class RouteTrackerApiContext : DbContext
 {
     public DbSet<Account> Accounts { get; init; } = null!;
     public DbSet<Setting> Settings { get; init; } = null!;
+    public DbSet<Route> Routes { get; init; } = null!;
 
     public RouteTrackerApiContext(DbContextOptions<RouteTrackerApiContext> options) : base(options)
     {
