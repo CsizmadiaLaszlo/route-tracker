046f532fe0868116ab86c8e8c8eb5b851cbd6c3b
László Csizmadia <csizilaci92@gmail.com>
2023-03-02 11:59:05 +0100

    Add Id property

--- route-tracker-api/Models/Route.cs
+++ route-tracker-api/Models/Route.cs
@@ -1,5 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+// ReSharper disable CollectionNeverUpdated.Global
+
 namespace route_tracker_api.Models;
 
 public class Route
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; init; }
 }
