57187ae83b56543da73731386492e089d0470b56
László Csizmadia <csizilaci92@gmail.com>
2023-03-02 21:00:21 +0100

    Add fetch to get routes for the day

--- route-tracker-spa/src/pages/Home.jsx
+++ route-tracker-spa/src/pages/Home.jsx
@@ -1,11 +1,25 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import DateSelector from "../components/home/DateSelector.jsx";
 import MockupWindow from "../components/shared/MockupWindow.jsx";
 import NewRoute from "../components/home/newRoute/NewRoute.jsx";
+import {useMsal} from "@azure/msal-react";
 
 export const Home = () => {
+    const {instance} = useMsal();
     const [date, setDate] = useState(new Date());
+    const [routes, setRoutes] = useState(null)
+    const [routesLoading, setRoutesLoading] = useState(false);
     const [saveClicked, setSaveClicked] = useState(false);
+
+    useEffect(() => {
+        setRoutesLoading(true);
+        const endpoint = `routes?date=${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+        fetchWithToken(instance, "GET", endpoint).then(r => {
+                setRoutes(r);
+                setRoutesLoading(false);
+            }
+        )
+    }, [saveClicked])
 
     return (
         <div className={"flex flex-wrap"}>
