8f1e7483985b0c18b9536a61422f2d02be708e07
László Csizmadia <csizilaci92@gmail.com>
2023-03-02 11:57:27 +0100

    Create Plate.cs

---
+++ route-tracker-api/Models/Plate.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+// ReSharper disable CollectionNeverUpdated.Global
+
+namespace route_tracker_api.Models;
+
+public class Plate
+{
+}
