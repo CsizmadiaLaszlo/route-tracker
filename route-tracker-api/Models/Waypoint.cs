68ac01cf5db228623a9403f090055d2fecf22d26
László Csizmadia <csizilaci92@gmail.com>
2023-03-02 12:00:37 +0100

    Add Id property

--- route-tracker-api/Models/Waypoint.cs
+++ route-tracker-api/Models/Waypoint.cs
@@ -1,3 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+// ReSharper disable CollectionNeverUpdated.Global
+
+namespace route_tracker_api.Models;
+
 public class Waypoint
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; init; }
 }
