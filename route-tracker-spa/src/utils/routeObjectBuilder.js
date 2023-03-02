f9adc7b98df0e6f18e42ae42afce839518d9cb24
László Csizmadia <csizilaci92@gmail.com>
2023-03-02 18:18:12 +0100

    Update routeObjectBuilder.js

--- route-tracker-spa/src/utils/routeObjectBuilder.js
+++ route-tracker-spa/src/utils/routeObjectBuilder.js
@@ -5,26 +5,6 @@ const routeObjectBuilder = (waypoints, plates, fromDate, toDate) => {
     const platesObjects = plates.map((plate) => {
         return {Name: plate}
     });
-    return {Waypoints: waypointsObjects, Plates: platesObjects, StartDate: fromDate, ToDate: toDate}
-    // return {
-    //     "Waypoints": [
-    //         {
-    //             "Name": "way1"
-    //         },
-    //         {
-    //             "Name": "way2"
-    //         }
-    //     ],
-    //     "Plates": [
-    //         {
-    //             "Name": "plate1"
-    //         },
-    //         {
-    //             "Name": "plate2"
-    //         }
-    //     ],
-    //     "StartDate": "2023-03-02T13:54:37.000Z",
-    //     "EndDate": "2023-03-02T13:54:37.000Z"
-    // }
+    return {Waypoints: waypointsObjects, Plates: platesObjects, StartDate: fromDate, EndDate: toDate}
 }
 export default routeObjectBuilder;
