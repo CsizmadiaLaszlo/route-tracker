bec2097481b33015a08bec4996e79b3d14675e97
László Csizmadia <csizilaci92@gmail.com>
2023-03-02 11:52:45 +0100

    Add AddRoute implementation

--- route-tracker-api/Services/AccountService.cs
+++ route-tracker-api/Services/AccountService.cs
@@ -2,6 +2,7 @@
 using route_tracker_api.Data;
 using route_tracker_api.Models;
 using route_tracker_api.Services.Interfaces;
+using Route = route_tracker_api.Models.Route;
 
 namespace route_tracker_api.Services;
 
@@ -27,32 +28,45 @@ public class AccountService : IAccountService
         var account = await GetAccountByOid(oid);
         if (account is not null)
         {
-            throw new InvalidOperationException();
+            throw new InvalidOperationException($"Unable to add account: account with oid: {oid}, already exist.");
         }
-        
-        await _context.Accounts.AddAsync(new Account() { ObjectIdentifier = oid, Setting = new Setting() });
+
+        await _context.Accounts.AddAsync(new Account() { ObjectIdentifier = oid });
         await _context.SaveChangesAsync();
     }
 
     public async Task<Setting> GetAccountSetting(string oid)
     {
         var account = await GetAccountByOid(oid);
-        if (account is null) throw new InvalidOperationException();
+        if (account is null)
+            throw new InvalidOperationException("Unable to retrieve account setting: account not found.");
         return account.Setting;
     }
 
     public async Task<Setting> UpdateAccountSetting(string oid, Setting newSetting)
     {
         var account = await GetAccountByOid(oid);
-        if (account is null) throw new InvalidOperationException();
+        if (account is null)
+            throw new InvalidOperationException("Unable to update account setting: account not found.");
         account.Setting.UpdateSetting(newSetting);
         await _context.SaveChangesAsync();
         return account.Setting;
     }
 
+    public async Task<Route> AddRoute(string oid, Route route)
+    {
+        var account = await GetAccountByOid(oid);
+        if (account is null) throw new InvalidOperationException("Unable to add route: account not found.");
+        account.Routes.Add(route);
+        await _context.SaveChangesAsync();
+        return route;
+    }
+
     private async Task<Account> GetAccountByOid(string oid)
     {
-        return (await _context.Accounts.Include(account => account.Setting)
+        return (await _context.Accounts
+            .Include(account => account.Setting)
+            .Include(account => account.Routes)
             .FirstOrDefaultAsync(account => account.ObjectIdentifier == oid))!;
     }
 }
