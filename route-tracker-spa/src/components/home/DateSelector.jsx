18cae63410fbb1a9ebb9345e50ed3a6164414386
László Csizmadia <csizilaci92@gmail.com>
2023-03-08 12:27:01 +0100

    Add Date global state

--- route-tracker-spa/src/components/home/DateSelector.jsx
+++ route-tracker-spa/src/components/home/DateSelector.jsx
@@ -1,13 +1,16 @@
-import {forwardRef} from "react";
+import {forwardRef, useContext, useEffect} from "react";
 import {useTranslation} from "react-i18next";
 import DatePicker from "react-datepicker";
 import {AiOutlineLeft, AiOutlineRight} from "react-icons/ai";
 import {modfiyeDateDay} from "../../utils/dateTools.js";
 import "react-datepicker/dist/react-datepicker.css";
 import setLocale from "../../utils/setLocale.js";
+import DateContext from "../../context/DateContext.jsx";
+import WeekContext from "../../context/WeekContext.jsx";
 
-const DateSelector = (props) => {
-    const {date, setDate} = props.date;
+const DateSelector = () => {
+    const {date, setDate} = useContext(DateContext)
+    const {daysOfWeek} = useContext(WeekContext)
     const {i18n, t} = useTranslation();
 
     const CustomInput = forwardRef(({value, onClick}, ref) => (
@@ -29,8 +32,11 @@ const DateSelector = (props) => {
     return (
         <DatePicker
             selected={date}
-            onChange={(date) => setDate(date)}
+            onChange={(date) => {
+                setDate(date);
+            }}
             locale={i18n.language}
+            highlightDates={daysOfWeek}
             customInput={<CustomInput/>}
         />
     );
--- route-tracker-spa/src/pages/Home.jsx
+++ route-tracker-spa/src/pages/Home.jsx
@@ -6,10 +6,14 @@ import fetchWithToken from "../utils/fetchWithToken.js";
 import {useMsal} from "@azure/msal-react";
 import {Progress} from "react-daisyui";
 import ShowRoutes from "../components/ShowRoutes.jsx";
+import DateContext from "../context/DateContext.jsx";
+import {getDaysOfWeek} from "../utils/dateTools.js";
+import WeekContext from "../context/WeekContext.jsx";
 
 export const Home = () => {
     const {instance} = useMsal();
     const [date, setDate] = useState(new Date());
+    const [daysOfWeek, setDaysOfWeek] = useState([]);
     const [routes, setRoutes] = useState(null)
     const [routesLoading, setRoutesLoading] = useState(false);
     const [saveClicked, setSaveClicked] = useState(false);
@@ -22,27 +26,37 @@ export const Home = () => {
                 setRoutesLoading(false);
             }
         )
-    }, [saveClicked])
+    }, [saveClicked, date])
+
+    useEffect(() => {
+        setDaysOfWeek(getDaysOfWeek(date))
+    }, [date])
 
     return (
-        <div className={"flex flex-wrap"}>
-            <div className={"w-2/3 flex-auto m-1"}>
-                <MockupWindow titleContainer={<DateSelector date={{date, setDate}}/>}>
-                    {routesLoading
-                        ?
-                        <Progress className="w-56"/>
-                        :
-                        routes
-                            ?
-                            <ShowRoutes routes={routes}/>
-                            :
-                            <></>
-                    }
-                </MockupWindow>
-            </div>
-            <div className={"w-80 m-1 flex-auto text-center"}>
-                <NewRoute date={date} saveClicked={saveClicked} setSaveClicked={setSaveClicked}/>
-            </div>
-        </div>
+        <DateContext.Provider value={{date, setDate}}>
+            <WeekContext.Provider value={{daysOfWeek, setDaysOfWeek}}>
+                <div className={"flex flex-wrap"}>
+                    <div className={"w-2/3 flex-auto m-1"}>
+                        <MockupWindow titleContainer={
+                            <DateSelector/>
+                        }>
+                            {routesLoading
+                                ?
+                                <Progress className="w-56"/>
+                                :
+                                routes
+                                    ?
+                                    <ShowRoutes routes={routes}/>
+                                    :
+                                    <></>
+                            }
+                        </MockupWindow>
+                    </div>
+                    <div className={"w-80 m-1 flex-auto text-center"}>
+                        <NewRoute saveClicked={saveClicked} setSaveClicked={setSaveClicked}/>
+                    </div>
+                </div>
+            </WeekContext.Provider>
+        </DateContext.Provider>
     );
 };
